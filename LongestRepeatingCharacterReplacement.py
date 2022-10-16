9e16a762996358ccb067f4b091bf38bd41026239
Osama Qutait <102440794+OsamaQutait@users.noreply.github.com>
2022-10-16 18:32:58 +0300

    done

--- LongestRepeatingCharacterReplacement.py
+++ LongestRepeatingCharacterReplacement.py
@@ -1,5 +1,22 @@
+import collections
 def characterReplacement(s: str, k: int) -> int:
-
-    return 0
+    max_len, a_pointer, b_pointer, max_freq_char = 0, 0, 0, 0
+    sub_string = collections.UserDict()
+    while a_pointer < len(s):
+        current_len = a_pointer - b_pointer + 1
+        if sub_string.__contains__(s[a_pointer]):
+            sub_string.__setitem__(s[a_pointer], sub_string.__getitem__(s[a_pointer])+1)
+        else:
+            sub_string.__setitem__(s[a_pointer], 1)
+        max_freq_char = max(max_freq_char, sub_string.__getitem__(s[a_pointer]))
+        if abs(current_len - max_freq_char) <= k:
+            max_len = max(max_len, current_len)
+        else:
+            sub_string.__setitem__(s[b_pointer], sub_string.__getitem__(s[b_pointer])-1)
+            if sub_string.__getitem__(s[b_pointer]) == 0:
+                sub_string.pop(s[b_pointer])
+            b_pointer += 1
+        a_pointer += 1
+    return max_len
 if __name__ == '__main__':
-    print(66)
+    print(characterReplacement("ABAA", 0))
