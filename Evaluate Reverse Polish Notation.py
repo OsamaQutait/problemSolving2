21d4a49ec4c785e3ac13eafac05ffaa906cbc72e
Osama Qutait <102440794+OsamaQutait@users.noreply.github.com>
2022-11-06 16:10:49 +0200

    done

---
+++ Evaluate Reverse Polish Notation.py
@@ -0,0 +1,24 @@
+import operator
+from typing import *
+def evalRPN(tokens: List[str]) -> int:
+    operation_dict = {
+        '+': operator.add,
+        '-': operator.sub,
+        '/': operator.truediv,
+        '*': operator.mul
+    }
+    stack = []
+    answer = 0
+    for item in tokens:
+        if operation_dict.__contains__(item):
+            answer = int(operation_dict.get(item)(int(stack[-2]), int(stack[-1])))
+            stack.pop()
+            stack.pop()
+            stack.append(answer)
+        else:
+            stack.append(item)
+    return stack[-1]
+
+if __name__ == '__main__':
+    print(evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
+
