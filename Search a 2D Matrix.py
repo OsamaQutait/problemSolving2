212cc451eeae7c384f57d1ddb022211fac91ef39
Osama Qutait <102440794+OsamaQutait@users.noreply.github.com>
2022-11-09 16:14:11 +0200

    done

--- Search a 2D Matrix.py
+++ Search a 2D Matrix.py
@@ -1,9 +1,18 @@
 from typing import List
 def searchMatrix(matrix: List[List[int]], target: int) -> bool:
-    rowSize, columnSize = len(matrix[0]), len(matrix)
-    for i in range(columnSize):
-        print(matrix[i][rowSize-1])
-    return True
+    row, col = len(matrix), len(matrix[0])
+    for i in range(row):
+        if matrix[i][col-1] >= target:
+            l, r = 0, col-1
+            while l <= r:
+                mid = l + (r - l) // 2
+                if matrix[i][mid] == target:
+                    return True
+                elif matrix[i][mid] < target:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+    return False
 
 if __name__ == '__main__':
-    print(searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3))
+    print(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13))
