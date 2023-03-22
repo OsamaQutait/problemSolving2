ebb9e891c7a4eaefbce500104add5464e2407696
Osama Qutait <102440794+OsamaQutait@users.noreply.github.com>
2023-03-22 19:39:28 +0200

    done

---
+++ NextGreaterElementII.py
@@ -0,0 +1,16 @@
+from typing import *
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        ans = [-1] * (len(nums))
+        n = len(nums)
+        stack = []
+        for i in range(n*2):
+            while stack and nums[i % n] > nums[stack[-1]]:
+                ans[stack.pop()] = nums[i % n]
+            if i < n:
+                stack.append(i)
+        return ans
+
+if __name__ == '__main__':
+    s = Solution
+    print(s.nextGreaterElements(None, [1, 2, 1]))
