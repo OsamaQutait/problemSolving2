aa748d9cb3e2e3df50821b45c46c06e3cbb39444
Osama Qutait <102440794+OsamaQutait@users.noreply.github.com>
2023-04-03 23:07:41 +0300

    done

--- Merge Two Sorted Lists.py
+++ Merge Two Sorted Lists.py
@@ -1,38 +1,41 @@
 from typing import *
+
+
 # node structure
 class ListNode:
-  def __init__(self, val = 0):
-    self.val = val
-    self.next = None
+    def __init__(self, val=0):
+        self.val = val
+        self.next = None
 
-#class Linked List
+
+# class Linked List
 class LinkedList:
-  def __init__(self):
-    self.head = None
+    def __init__(self):
+        self.head = None
 
-  #Add new element at the end of the list2
-  def push_back(self, newElement):
-    newNode = ListNode(newElement)
-    if(self.head == None):
-      self.head = newNode
-      return
-    else:
-      temp = self.head
-      while(temp.next != None):
-        temp = temp.next
-      temp.next = newNode
+    # Add new element at the end of the list2
+    def push_back(self, newElement):
+        newNode = ListNode(newElement)
+        if (self.head == None):
+            self.head = newNode
+            return
+        else:
+            temp = self.head
+            while (temp.next != None):
+                temp = temp.next
+            temp.next = newNode
 
-  #display the content of the list2
-  def PrintList(self):
-    temp = self.head
-    if(temp != None):
-      print("The list2 contains:", end=" ")
-      while (temp != None):
-        print(temp.val , end=" ")
-        temp = temp.next
-      print()
-    else:
-      print("The list2 is empty.")
+    # display the content of the list2
+    def PrintList(self):
+        temp = self.head
+        if (temp != None):
+            print("The list2 contains:", end=" ")
+            while (temp != None):
+                print(temp.val, end=" ")
+                temp = temp.next
+            print()
+        else:
+            print("The list2 is empty.")
 
 
 class Solution:
@@ -54,16 +57,16 @@ class Solution:
 
 
 if __name__ == '__main__':
-  s = Solution()
-  list1 = LinkedList()
-  list1.push_back(1)
-  list1.push_back(3)
-  list1.push_back(5)
+    s = Solution()
+    list1 = LinkedList()
+    list1.push_back(1)
+    list1.push_back(3)
+    list1.push_back(5)
 
-  list2 = LinkedList()
-  list2.push_back(2)
-  list2.push_back(4)
-  list2.push_back(6)
+    list2 = LinkedList()
+    list2.push_back(2)
+    list2.push_back(4)
+    list2.push_back(6)
 
-  list1.head =s.mergeTwoLists(list1.head, list2.head)
-  list1.PrintList()
+    list1.head = s.mergeTwoLists(list1.head, list2.head)
+    list1.PrintList()
--- Reverse Linked List.py
+++ Reverse Linked List.py
@@ -1,39 +1,43 @@
 from typing import *
+
+
 # node structure
 class Node:
-  def __init__(self, data):
-    self.data = data
-    self.next = None
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-#class Linked List
+
+# class Linked List
 class LinkedList:
-  def __init__(self):
-    self.head = None
+    def __init__(self):
+        self.head = None
 
-  #Add new element at the end of the list2
-  def push_back(self, newElement):
-    newNode = Node(newElement)
-    if(self.head == None):
-      self.head = newNode
-      return
-    else:
-      temp = self.head
-      while(temp.next != None):
-        temp = temp.next
-      temp.next = newNode
+    # Add new element at the end of the list2
+    def push_back(self, newElement):
+        newNode = Node(newElement)
+        if (self.head == None):
+            self.head = newNode
+            return
+        else:
+            temp = self.head
+            while (temp.next != None):
+                temp = temp.next
+            temp.next = newNode
 
-  #display the content of the list2
-  def PrintList(self):
-    temp = self.head
-    if(temp != None):
-      print("The list2 contains:", end=" ")
-      while (temp != None):
-        print(temp.val, end=" ")
-        temp = temp.next
-      print()
-    else:
-      print("The list2 is empty.")
-      
+    # display the content of the list2
+    def PrintList(self):
+        temp = self.head
+        if (temp != None):
+            print("The list2 contains:", end=" ")
+            while (temp != None):
+                print(temp.val, end=" ")
+                temp = temp.next
+            print()
+        else:
+            print("The list2 is empty.")
+
+
 class Solution:
     def reverseList(self, head: Optional[Node]) -> Optional[Node]:
         prev, curr = None, head
@@ -43,7 +47,6 @@ class Solution:
             prev = curr
             curr = n
         return prev
-
 
 
 if __name__ == '__main__':
