7492e100034f39f2d528e6c594181201e29606d4
Osama Qutait <102440794+OsamaQutait@users.noreply.github.com>
2023-04-05 00:28:53 +0300

    done

---
+++ Middle of the Linked List.py
@@ -0,0 +1,62 @@
+from typing import *
+
+import self as self
+
+
+# node structure
+class ListNode:
+    def __init__(self, val=0):
+        self.val = val
+        self.next = None
+
+
+# class Linked List
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    # Add new element at the end of the list2
+    def push_back(self, newElement):
+        newNode = ListNode(newElement)
+        if (self.head == None):
+            self.head = newNode
+            return
+        else:
+            temp = self.head
+            while (temp.next != None):
+                temp = temp.next
+            temp.next = newNode
+
+    # display the content of the list2
+    def PrintList(self):
+        temp = self.head
+        if (temp != None):
+            print("The list2 contains:", end=" ")
+            while (temp != None):
+                print(temp.val, end=" ")
+                temp = temp.next
+            print()
+        else:
+            print("The list1 is empty.")
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head.next: return head
+        slow, fast = head, head.next
+        while fast.next and fast.next.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow.next
+if __name__ == '__main__':
+    s = Solution()
+    list1 = LinkedList()
+    list1.push_back(1)
+    # list1.push_back(3)
+    # list1.push_back(5)
+    # list1.push_back(3)
+    # list1.push_back(1)
+    # list1.push_back(1)
+    list1.PrintList()
+    list1.head = s.middleNode(list1.head)
+    list1.PrintList()
+
